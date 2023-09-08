152f88dd7d8d239c7bb40d55e3ddd74c77a20e7c
mrprotocoll <lekanvgbg@gmail.com>
2023-09-08 09:23:35 +0100

    update users endpoint

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class)->withTimestamps();
     }
 
-    public function current() {
+    public static function current() {
         return Auth::user();
     }
 

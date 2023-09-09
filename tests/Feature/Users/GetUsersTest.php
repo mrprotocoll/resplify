28d9e709047860c8d16c2e38fda50c4224756669
mrprotocoll <lekanvgbg@gmail.com>
2023-09-09 22:26:39 +0100

    rename user store test

--- tests/Feature/Users/GetUsersTest.php
+++ tests/Feature/Users/GetUsersTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class StoreUserTest extends TestCase
+class GetUsersTest extends TestCase
 {
     /**
      * A basic feature test example.

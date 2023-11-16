2521e088932b04d7dca91fddb0b162f51b0869d0
mrprotocoll <lekanvgbg@gmail.com>
2023-11-17 00:45:35 +0100

    refactored admin remark and category

--- app/Http/Controllers/Api/V1/Admin/CategoryController.php
+++ app/Http/Controllers/Api/V1/Admin/CategoryController.php
@@ -8,31 +8,51 @@ use App\Http\Requests\V1\CategoryRequest;
 use App\Http\Resources\V1\CategoryResource;
 use App\Models\Admin;
 use App\Models\Category;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
 
     public function index()
     {
-        $categories = Category::all();
-        return ResponseHelper::success(CategoryResource::collection($categories));
+        try {
+            $categories = Category::all();
+            return ResponseHelper::success(CategoryResource::collection($categories));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 
     public function store(CategoryRequest $request)
     {
-
-        $category = Admin::current()->categories()->create($request->validated());
-        return $category ? ResponseHelper::success(new CategoryResource($category)) : ResponseHelper::error();
+        try {
+            $category = Admin::current()->categories()->create($request->validated());
+            return ResponseHelper::success(new CategoryResource($category));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 
     public function show(Category $category)
     {
-        return $category ? ResponseHelper::success(new CategoryResource($category)) : ResponseHelper::error();
+        try {
+            return ResponseHelper::success(new CategoryResource($category));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 
     public function update(CategoryRequest $request, Category $category)
     {
-        $update = $category->update($request->validated());
-        return $update ? ResponseHelper::success(new CategoryResource($category)) : ResponseHelper::error();
+        try {
+            $category->update($request->validated());
+            return ResponseHelper::success(new CategoryResource($category));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 }
--- app/Http/Controllers/Api/V1/Admin/RemarkController.php
+++ app/Http/Controllers/Api/V1/Admin/RemarkController.php
@@ -12,6 +12,7 @@ use App\Models\Admin;
 use App\Models\Remark;
 use App\Models\User;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class RemarkController extends Controller
@@ -19,36 +20,49 @@ class RemarkController extends Controller
 
     public function store(RemarkRequest $request)
     {
-        $remark = new Remark();
-        $remark->created_by = Admin::current()->id;
-        $remark->name = $request->name;
-        $remark->description = $request->description;
-        $image = $request->file('image');
-        if($image) {
-            $remark->image = FileHelper::upload($image, 'remarks');
-        }
+        try {
+            $remark = new Remark();
+            $remark->created_by = Admin::current()->id;
+            $remark->name = $request->name;
+            $remark->description = $request->description;
+            $image = $request->file('image');
+            if($image) {
+                $remark->image = FileHelper::upload($image, 'remarks');
+            }
 
-        $saved = $remark->save();
-        return $saved ? ResponseHelper::success(new RemarkResource($remark)) : ResponseHelper::error();
+            $remark->save();
+            return ResponseHelper::success(new RemarkResource($remark));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 
     public function update(RemarkRequest $request, Remark $remarks)
     {
-        $remarks->name = $request->name;
-        $remarks->description = $request->description;
-        $image = $request->file('image');
-        if($image) {
-            $remarks->image = FileHelper::upload($image, 'remarks');
+        try {
+            $remarks->name = $request->name;
+            $remarks->description = $request->description;
+            $image = $request->file('image');
+            if($image) {
+                $remarks->image = FileHelper::upload($image, 'remarks');
+            }
+            return ResponseHelper::success(new RemarkResource($remarks));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
         }
-        return $remarks->save() ? ResponseHelper::success(new RemarkResource($remarks)) : ResponseHelper::error();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Remark $remarks)
     {
-        $deleted = $remarks;
-        return $remarks->delete() ? ResponseHelper::success(new RemarkResource($deleted)) : ResponseHelper::error();
+        try {
+            $deleted = $remarks;
+            $remarks->delete();
+            return ResponseHelper::success(new RemarkResource($deleted));
+        }catch (\Exception $e) {
+            Log::error($e);
+            return ResponseHelper::error();
+        }
     }
 }
